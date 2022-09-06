c583e15fe409d7e9fdf669c331a42e684668156a
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-06 19:58:05 -0300

    Construindo Schema do usuário

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,4 +1,27 @@
 //importando biblioteca mongoose
 const mongoose = require('mongoose');
+//importando ferramenta Schema
+const Schema =  mongoose.Schema;
 
-const schema =  mongoose.Schema;
+//mensagem de erro para campos required
+const erroRequired = '*Campo obrigatório!';
+
+const UsuarioSchema = new Schema ({
+    //definição do nome usando objeto tipo json
+    nome: {
+        //tipo de dado que será armazenado
+        type: String,
+        //definindo como obrigatório e definindo mensagem de erro
+        required: [true, erroRequired]
+    },
+
+    email: {
+        type: String,
+        required: [true, erroRequired]
+    },
+
+    senha: {
+        type: String,
+        required: [true, erroRequired]
+    }
+})
