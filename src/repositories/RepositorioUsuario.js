1668f49a6409cd4c542183ded74704b23ef6a312
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-07 07:22:08 -0300

    criando repositorio de implementação do usuario

--- src/repositories/RepositorioUsuario.js
+++ src/repositories/RepositorioUsuario.js
@@ -1,3 +1,10 @@
 // faz a definição da interface do repositório de usuários
 //qualquer implementação de repositório de usuario vai precisar ter os métodos definidos aqui
 
+module.exports = (Implementacao) => {
+    //se a implementacao não tiver um método cadastrar, vai lançar um erro
+    if (!Implementacao.cadastrar) {
+        throw new Error(`A classe ${Implementacao} não implementou o método cadastrar!`)
+    }
+    return Implementacao;
+}
