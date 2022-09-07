0cfdbb48faf32477f0107823c38930d3f3da54e2
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-07 11:17:51 -0300

    criando método logar, definindo gerador de token jwt

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -1,8 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 class LoginService{
+//TODO: verificar se o usuário está cadastrado no banco de dados
+
     //criando o método para realizar o login, deve ser público para poder ser usado em vários lugares
     logar (login, senha) {
+        const usuario = {
+            id: 1
+        }
+        //gera o token de acesso usando o JWT
+        jwt.sign({
+            //usa o _ pra coisas que são usadas apenas dentro da aplicação
+            _id: usuario.id
+        }, process.env.CHAVE_SECRETA_JWT) //variável de ambiente que terá a chave para descriptografar o token
+        
+        //devolve as informações do usuário autenticado com o seu token de acesso
         return{
             id: 1,
             nome: 'usuariox',
