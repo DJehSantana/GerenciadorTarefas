4c16eabd10ba8383a58de6300490af2c5afe3d28
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-07 19:29:38 -0300

    verificawndo autorização de acesso do token

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,3 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+//define a lista de rotas publicas da aplicação
+const rotasPublicas  = {
+
+}
+
 module.exports = (req, res, next) => {
-    req.logger.info('Verificando permissão de acesso a rota', `rota = ${req.url}`);
+    req.logger.info('Verificando permissão de acesso a', `rota: ${req.url}`);
+
+    //headers usado para acessar o header do BD
+    const authorization = req.headers.authorization;
+
+    //envia uma mensagem de erro 401 se o header não for enviado
+    if (!authorization) {
+        res.status(401).json({
+            status: 401,
+            erro: 'acesso negado, você precisa enviar o header authorization'
+        });
+        
+        console.log('Falha na autorização');
+        return res;
+    }
+
+    next();
 }
