6fa54cb140eead6d541270ab1e122151573395a9
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-15 20:08:19 -0300

    instanciando o serviço do usuário no controller, verificando se o cadastro foi realizado com sucesso no BD

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -9,12 +9,36 @@ class UsuarioController extends HttpController{
         this.express.post(`${baseUrl}/usuario`, this.cadastrar.bind(this));
     }
 
-    cadastrar(req, res) {
+    async cadastrar(req, res) {
         //receber dados do usuário para cadastro
         const dadosUsuario = req.body;
-        //garantir o recebimento da requisição no método cadastrar 
-        req.logger.info('Usuário cadastrado com sucesso!');
-        res.json(dadosUsuario);
+        //qualquer erro ou exceção que acontecer será tratada e devolvida para o usuário
+        try {
+            //instanciar o usuarioservice
+            const servico = new UsuarioService();
+            const retornoServico = await servico.cadastrar(dadosUsuario);
+
+            //verificar se cadastro foi bem sucedido
+            if (retornoServico.erros) {
+                return res.status(400).json({
+                    status: 400,
+                    //o join vai transformar o array em uma string unica separada por virgula
+                    erro: retornoServico.erros.join(',')
+                })
+            }
+            //garantir o recebimento da requisição no método cadastrar 
+            req.logger.info('Usuário cadastrado com sucesso!');
+            //devolve uma mensagem quando o cadastro tiver sucesso
+            res.json({
+                msg: 'Usuário criado com sucesso no banco de dados'
+            });
+            
+        } catch (error) {
+            req.logger.error('Erro ao cadastrar usuário, error= ' + error.message);
+            res.status(500).json({
+                erro: 'Problema ao cadastrar usuáro! Tente novamente mais tarde!'
+            })
+        }
     }
 
 
