a1652d63668103f32763baa4cb9443442a20c886
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-16 22:50:27 -0300

    criando construtor da classe Tarefa service

---
+++ src/services/TarefaService.js
@@ -0,0 +1,14 @@
+class TarefaService {
+    constructor(idUsuario) {
+        //atribui o Id do usuario recebido por parâmetro a uma propriedade da classe
+        this.idUsuario = idUsuario;
+    }
+
+    //método responsável por fazer o filtro das tarefas no banco de dados
+    async filtrar() {
+        
+
+    }
+}
+
+module.exports = TarefaService;
