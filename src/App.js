0b10103192f2b61dff15fc58810a4f3ce5a58760
DJehSantana <jessicah.dias23@hotmail.com>
2022-11-14 16:05:40 -0300

    ajuste de porta

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App {
     #iniciarServidor = () => {
         //tenta pegar a porta a partir da variável de ambiente EXPRESS_PORT
         //caso esta não esteja definida, usa a porta padrão 3001
-        const porta = process.env.EXPRESS_PORT || 3002;
+        const porta = process.env.PORT || 3000;
         //listen - recebe como parâmetro a porta e um callback 
         this.express.listen(porta, () => {
             //mensagem de sucesso caso o express consiga se conectar a porta
