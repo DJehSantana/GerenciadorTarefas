8d60afa2ea61103c5ae20e0c092ce2283e861a27
DJehSantana <jessicah.dias23@hotmail.com>
2022-11-14 20:07:33 -0300

    teste heroku

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -7,6 +7,10 @@ const rotasPublicas  = [
         url: '/api/login',
         metodo: 'POST'
     },
+    {
+        url: '/*',
+        metodo: 'GET'
+    },
     //rota do swagger
     {
         url: '/api/docs*',
